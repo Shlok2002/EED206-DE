d40b5de01a642649d22924c1423717603f29b1f5
SourLuck <75850979+SourLuck@users.noreply.github.com>
2021-02-18 12:17:20 +0530

    Update Exp_05b.sv

--- Exp_05b.sv
+++ Exp_05b.sv
@@ -25,7 +25,7 @@ end
 //TOP MODULE FUNCTIONS
 
 logic P,Q,R,S,A,B,C,D;
-Decryption Decrypt(P,Q,R,S,A,B,C,D);
+Decryption_circuit Decrypt(P,Q,R,S,D,C,B,A);
 
 //DEFINING INPUTS
  
@@ -35,7 +35,16 @@ Decryption Decrypt(P,Q,R,S,A,B,C,D);
 	Q = in[1];
 	R = in[2];
 	S = in[3];
-#5initial begin
+	 
+#5
+ 	out[0] = D;
+	out[1] = C;
+	out[2] = B;
+	out[3] = A;
+
+end
+
+initial begin
 
 //Generating output text file
 
@@ -46,14 +55,50 @@ Decryption Decrypt(P,Q,R,S,A,B,C,D);
 end
 endmodule  //End of Testbench
 
-	out[0] = A;
-	out[1] = C;
-	out[2] = A;
-	out[3] = D;
+//DESIGN - DECRYPTION CIRCUIT
 
-end
+module and_4in(and4_out,a,b,c,d); //4_input AND gate
+   	input a,b,c,d;
+  	wire t1,t2;
+   	output and4_out;
 
+   	and	and1(t1,a,b),
+       		and2(t2,c,d),
+      		and3(and4_out,t1,t2);
+endmodule // End of 4-input AND gate module
 
+module or_4in(or4_out,a,b,c,d); //4_input OR gate
+   	input a,b,c,d;
+  	wire t1,t2;
+   	output or4_out;
 
+   	or	and1(t1,a,b),
+       		and2(t2,c,d),
+		and3(or4_out,t1,t2);
+endmodule // End of 4-input OR gate module
+
+module Decryption_circuit(P,Q,R,S,D,C,B,A);
+	input P,Q,R,S;
+	wire w1,w2,w3,w4,w5,w6,w7,PN,QN,RN,SN;
+	output D,C,B,A;
+
+	and_4in an1(w1,PN,RN,S,1'b1),
+	an2(w3,PN,RN,SN,1'b1);
+	
+	or_4in or1(C,w3,w4,w5,1'b0),
+	or2(B,w3,w6,w7,1'b0);
+	and	a1(w2,Q,R),
+	a2(w4,PN,QN),
+	a3(w5,QN,RN),
+	a4(w6,P,R),
+	a5(w7,R,S);
+	or	o1(D,w1,w2),
+	o2(A,SN,1'b0);
+	not	n1(PN,P),
+		n2(QN,Q),
+		n3(RN,R),
+		n4(SN,S);
+
+endmodule
 
 
